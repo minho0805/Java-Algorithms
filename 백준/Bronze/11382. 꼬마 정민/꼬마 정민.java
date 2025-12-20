518caa35d528ec87a46fc898b8bf89cebd164db0
minho0805 <2022305011@skuniv.ac.kr>
2025-12-20 21:20:42 +0900

    [Bronze V] Title: 꼬마 정민, Time: 164 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long A = input.nextLong();
+        long B = input.nextLong();
+        long C = input.nextLong();
+        long sum = A + B + C;
+        System.out.print(sum);
+
+    }
+}
